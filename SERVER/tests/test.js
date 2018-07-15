d3abb5b1d8e1eeac767a772f76f3d9d72b9e0f84
Seun Odewale <seunodewale.zone5@gmail.com>
2018-07-15 11:05:48 -0700

    add test for POST /diaries

--- SERVER/tests/test.js
+++ SERVER/tests/test.js
@@ -2,9 +2,14 @@ import chai, { request, expect } from 'chai';
 import chaiHttp from 'chai-http';
 
 import { app } from '../app';
-// import diaries from '../models/data';
+import diaries from '../models/data';
 
 chai.use(chaiHttp);
+
+// beforeEach((done) => {
+//   retdiaries
+//   done();
+//   });
 
 describe('API Endpoints', () => {
   it('should list all diaries on GET /diaries', (done) => {
@@ -16,7 +21,26 @@ describe('API Endpoints', () => {
         done();
       });
   });
-  it('should add get a specific diary on GET /diaries/:id');
-  it('should add a new diary on POST /diaries');
+  it('should get a specific diary on GET /diaries/:id');
+  // , (done) => {
+  // request(app)
+  //   .get('/diaries/:id')
+  //   .end((err, res) => {
+  //     expect(res).to.be.an('object');
+  //     done();
+  //   }); }
+
+  it('should add a new diary on POST /diaries', (done) => {
+    request(app)
+      .post('/api/diaries')
+      .send({
+        title: 'bar U Stasi',
+        content: 'Easy to prepare',
+      })
+      .end((err, res) => {
+        expect(res).to.be.an('object');
+        done();
+      });
+  });
   it('should modify a specific diary on PUT /diaries/:id');
 });
