64ea7c6d4864a6cfba5284347300e62be59452c1
Seun Odewale <seunodewale.zone5@gmail.com>
2018-07-19 10:47:43 -0700

    restructure files and folders

---
+++ SERVER/routes/route.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import Diary from '../controller/diaries';
+
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200)
+    .send({
+      message: 'Welcome to MyDiary API',
+    });
+});
+
+router.get('/diaries', Diary.getDiary);
+router.get('/diaries/:id', Diary.getSpecificDiary);
+router.post('/diaries', Diary.createDiary);
+router.put('/diaries/:id', Diary.updateDiary);
+
+export default router;
