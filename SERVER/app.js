f9e308ac3e1491f47092fd9eae5ac23b01efdf48
Seun Odewale <seunodewale.zone5@gmail.com>
2018-07-19 10:32:16 -0700

    restructure files and folders

--- SERVER/app.js
+++ SERVER/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 
-import Diary from './controller/diaries';
+import router from './routes/route';
 
 const PORT = 3000;
 const app = express();
@@ -12,16 +12,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Setup a default route that sends back a welcome message in JSON format.
-app.get('/', (req, res) => res.status(200).send({
-  message: 'Welcome to MyDiary API',
-}));
-
-// API routes
-app.get('/diaries', Diary.getDiary);
-app.get('/diaries/:id', Diary.getSpecificDiary);
-app.post('/diaries', Diary.createDiary);
-app.put('/diaries/:id', Diary.updateDiary);
+app.use('/api/v1', router);
 
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
 
