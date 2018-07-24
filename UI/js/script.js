aaa4b90892f6d461d607de59227c52cb35fa7af1
Seun Odewale <seunodewale.zone5@gmail.com>
2018-07-23 23:16:24 -0700

    removed lints in script.js

--- UI/js/script.js
+++ UI/js/script.js
@@ -11,33 +11,29 @@ const displayForm = () => {
   signUp.classList.toggle('hide');
   signIn.classList.toggle('hide');
 };
-	
-//Get  all h3 target element 
-h3.forEach(h3Element => {
- //Add Click event listener to all h3 tags
-  h3Element.addEventListener("click", function(e){
-        //use event e to get the specific element that was clicked
-        e.target.nextElementSibling.classList.toggle('hide')
-   	 })
- 	})
 
-	 if(cancel) {
-        cancel.addEventListener('click', function() {
-			modal.classList.toggle('hide');
-		}) 
-	}
-	
-	if(register) {
-        register.onclick = displayForm;
-    }
-    
-    if(login){
-        login.onclick = displayForm;
-	}
-	
-	edits.forEach(edit => {
-	   edit.addEventListener("click", function(e){
-		   modal.classList.toggle('hide')
-	   })
-	})
-   
+h3.forEach((h3Element) => {
+  h3Element.addEventListener('click', (e) => {
+    e.target.nextElementSibling.classList.toggle('hide')
+  });
+});
+
+if (cancel) {
+  cancel.addEventListener('click', () => {
+    modal.classList.toggle('hide');
+  });
+}
+
+if (register) {
+  register.onclick = displayForm;
+}
+
+if (login) {
+  login.onclick = displayForm;
+}
+
+edits.forEach((edit) => {
+  edit.addEventListener('click', () => {
+    modal.classList.toggle('hide');
+  });
+});
