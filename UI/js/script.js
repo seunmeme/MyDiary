8a15eb4abbbc1b0ce66c11549842edf1e0ab1628
Seun Odewale <seunodewale.zone5@gmail.com>
2018-07-23 23:04:32 -0700

    fixed bugs with cancel button in modal

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,28 +1,33 @@
+const login = document.getElementById('login');
+const signIn = document.getElementById('signIn');
+const signUp = document.getElementById('signUp');
+const register = document.getElementById('register');
+const cancel = document.getElementById('cancel');
+const h3 = document.querySelectorAll('h3');
+const modal = document.getElementById('cover-div');
+const edits = document.querySelectorAll('span');
 
-	 const login = document.getElementById('login');
-	 const signIn = document.getElementById('signIn');
-     const signUp = document.getElementById('signUp');
-     const register = document.getElementById('register');
-     const main = document.getElementById('main');
-     const h3 = document.querySelectorAll('h3');
-	 const modal = document.getElementById('cover-div');
-	 const edits = document.querySelectorAll('span');
-
- let displayForm = () => {
-    signUp.classList.toggle("hide");
-    signIn.classList.toggle("hide");
-	}	
+const displayForm = () => {
+  signUp.classList.toggle('hide');
+  signIn.classList.toggle('hide');
+};
 	
 //Get  all h3 target element 
- 	h3.forEach(h3Element => {
-     //Add Click event listener to all h3 tags
-    	h3Element.addEventListener("click", function(e){
+h3.forEach(h3Element => {
+ //Add Click event listener to all h3 tags
+  h3Element.addEventListener("click", function(e){
         //use event e to get the specific element that was clicked
         e.target.nextElementSibling.classList.toggle('hide')
    	 })
  	})
 
-    if(register){
+	 if(cancel) {
+        cancel.addEventListener('click', function() {
+			modal.classList.toggle('hide');
+		}) 
+	}
+	
+	if(register) {
         register.onclick = displayForm;
     }
     
